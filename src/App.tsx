f5a331ece5a3fa93d3e432d7eba6ce3bef50de92
Zhizhou Ma <zm9913@gmail.com>
2020-08-06 16:34:29 +0930

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { Space, Button } from 'antd';
 import { Switch } from 'antd';
+import {LeftOutlined, RightOutlined} from '@ant-design/icons';
+import Gallery from './components/Gallery';
 
 const App = () => {
   const [enable, setEnable] = useState(true);
+  const [colour, setColour] = useState("red");
 
   return (
-    <Space direction="vertical" size={12}>
-      <Switch defaultChecked disabled={enable} />
+    <Space direction="vertical" size={12} align="center" style={{width:"300px"}}>
+      <Switch defaultChecked checkedChildren="hi" disabled={enable} />
       <Button onClick={() => setEnable(!enable)}>Change</Button>
+      <Button type="ghost" icon={<LeftOutlined/>} />
+      <Gallery/>
     </Space>
   );
 };
